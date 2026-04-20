bb16b4fb097c5d4de67dee3daf8e60b070308756
gustavosbarroso <gustavo.s.barroso@gmail.com>
2026-04-20 11:04:34 -0300

    Create Question_3_Simpson's_rule.py

---
+++ Chapter 5/Question_3_Simpson's_rule.py
@@ -0,0 +1,20 @@
+#Imports
+import numpy as np
+import matplotlib.pyplot as plt
+#Função
+def f(x):
+  return np.exp(-x**2)
+#Integração
+a=0.0
+b=2.0
+N=100
+h=(b-a)/N
+s=(f(a) + f(b))
+for k in range (1,N):
+  if k%2==0:
+    s+= 2*f(a + k*h)
+  else:
+    s+= 4*f(a + k*h)
+resultado =(h*s)/3
+#Resultado
+print(resultado)
