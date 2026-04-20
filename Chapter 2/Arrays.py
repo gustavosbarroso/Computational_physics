ac9895972e758bf1399b7f3d46c82d677fedece5
gustavosbarroso <gustavo.s.barroso@gmail.com>
2026-04-20 10:57:50 -0300

    Update Arrays.py

--- Chapter 2/Arrays.py
+++ Chapter 2/Arrays.py
@@ -4,3 +4,6 @@ b = array([2,4,6,8] ,int)
 print(b/a+1)
 print(b/(a+1))
 print(1/a)
+#No caso (b/a + 1), divide-se o elemento b[1] por a[1] e assim por diante e depois soma 1 em todos os elementos do array.#
+#No caso 2,o parênteses prioriza a soma a+1 em relação a multiplicação. a+1 é feito e a divisão de elemento por elemento é feita.#
+#No caso 3, 1 divide cada elemento do array individualmente. #
